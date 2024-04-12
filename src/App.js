5e941b7ec8daaec97420c6d7ac34a8969bfb5f14
eoekstpal <park.se.mi47@gmail.com>
2024-04-12 22:21:17 +0900

    reset css 파일 수정, global font (black han sans) 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
 import './App.css';
 import Header from './compnents/Header';
 
+// global font 적용
+const GlobalStyle = createGlobalStyle`
+  body{
+    font-family: 'Black Han Sans';
+  }
+  `;
 
 function App() {
   return (
     <>
-    <Header />
+      <GlobalStyle />
+      <Header />
     </>
   );
 }
