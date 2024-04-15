88ad060098f619a8247da69d08c2c1db264656fa
eoekstpal <park.se.mi47@gmail.com>
2024-04-16 06:41:41 +0900

    새로고침시 페이지 최상단으로 이동

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 import PageLayout from './components/PageLayout';
 
 function App() {
+
+  useEffect(() => { 
+    window.onbeforeunload = function pushRefresh() {
+        window.scrollTo(0, 0);
+      };
+    }, []);
+
   return (
     <>
       <PageLayout />
